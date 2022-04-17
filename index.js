f7a97876bbc98a7b6763b86a18c04d5adeed5757
Locaided <locaided@example.com>
2022-04-17 18:01:51 +0500

    Commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5002;
+const path = require("path");
 
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/user");
@@ -11,5 +12,9 @@ app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 
+app.use(express.static(path.join(__dirname, "client", "build")))
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
