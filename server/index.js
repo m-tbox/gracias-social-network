256081ffa998397959a97c1ec8202ff9adb605bb
Locaided <locaided@example.com>
2022-04-17 17:46:31 +0500

    Deploying fixes

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5002;
-const path = require("path");
 
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/user");
@@ -12,12 +11,5 @@ app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 
-
-if (process.env.NODE_ENV === "production" || process.env.NODE_ENV === "staging") {
-    app.use(express.static("client / build"));
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname + "/client/build/index.html"));
-    });
-}
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
